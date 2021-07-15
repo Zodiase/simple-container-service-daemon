c834e5874575364bad6241bbf5b75321f5102fbf
Xingchen Hong (Xingchen@Onfleet) <xingchen@onfleet.com>
2021-07-15 04:25:31 -0700

    Use 303 redirect to use GET.

--- src/Reception.ts
+++ src/Reception.ts
@@ -63,7 +63,7 @@ export function createReception({ port, onStatus, onStart, onStop, onRestart }: 
             return;
         }
 
-        res.redirect('/');
+        res.redirect(303, '/');
     });
 
     app.post('/stop', async (req, res) => {
@@ -74,13 +74,13 @@ export function createReception({ port, onStatus, onStart, onStop, onRestart }: 
             return;
         }
 
-        res.redirect('/');
+        res.redirect(303, '/');
     });
 
     app.post('/restart', async (req, res) => {
         await onRestart();
 
-        res.redirect('/');
+        res.redirect(303, '/');
     });
 
     app.listen(port, () => {
